ba71772c9ba12f56ee38a5288826af058fe737f1
stayRealLang <meantime-imac@mingtongIMac.local>
2015-12-02 17:56:30 +0800

    1
    firstCommit

---
+++ URLTextField.h
@@ -0,0 +1,13 @@
+//
+//  URLTextField.h
+//  Advertising
+//
+//  Created by 明通imac on 15/11/19.
+//  Copyright (c) 2015年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface URLTextField : UITextField
+
+@end
---
+++ URLTextField.m
@@ -0,0 +1,108 @@
+//
+//  URLTextField.m
+//  Advertising
+//
+//  Created by 明通imac on 15/11/19.
+//  Copyright (c) 2015年 Aaron. All rights reserved.
+//
+
+#import "URLTextField.h"
+
+@implementation URLTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (id)initWithCoder:(NSCoder *)aDecoder{
+    self=[super initWithCoder:aDecoder];
+    if(self){
+        UIView*aView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 40)];
+        aView.backgroundColor=SECTIONCOLOR;
+        
+        UIButton *httpButton = [[UIButton alloc]initWithFrame:CGRectMake(10, 0, (SCREEN_WIDTH-40)/4, 40)];
+        [httpButton setTitle:@"http://" forState:UIControlStateNormal];
+        [httpButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [httpButton addTarget:self action:@selector(addHttp) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:httpButton];
+        
+        UIButton *wwwButton = [[UIButton alloc]initWithFrame:CGRectMake(20+(SCREEN_WIDTH-40)/4, 0, (SCREEN_WIDTH-40)/3, 40)];
+        [wwwButton setTitle:@"www." forState:UIControlStateNormal];
+        [wwwButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [wwwButton addTarget:self action:@selector(addwww) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:wwwButton];
+        
+        UIButton *comButton = [[UIButton alloc]initWithFrame:CGRectMake(30+2*(SCREEN_WIDTH-40)/4, 0, (SCREEN_WIDTH-40)/3, 40)];
+        [comButton setTitle:@".com" forState:UIControlStateNormal];
+        [comButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [comButton addTarget:self action:@selector(addcom) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:comButton];
+        
+        UIButton *cnButton = [[UIButton alloc]initWithFrame:CGRectMake(40+3*(SCREEN_WIDTH-40)/4, 0, (SCREEN_WIDTH-40)/3, 40)];
+        [cnButton setTitle:@".cn" forState:UIControlStateNormal];
+        [cnButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [cnButton addTarget:self action:@selector(addcn) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:cnButton];
+        
+        self.inputAccessoryView=aView;
+        
+    }
+    return self;
+
+}
+-(id)init{
+    self=[super init];
+    if(self){
+        UIView*aView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 50)];
+        aView.backgroundColor=SECTIONCOLOR;
+        
+        UIButton *httpButton = [[UIButton alloc]initWithFrame:CGRectMake(10, 5, (SCREEN_WIDTH-40)/4, 40)];
+        [httpButton setTitle:@"http://" forState:UIControlStateNormal];
+        [httpButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [httpButton addTarget:self action:@selector(addHttp) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:httpButton];
+        
+        UIButton *wwwButton = [[UIButton alloc]initWithFrame:CGRectMake(20+(SCREEN_WIDTH-40)/4, 5, (SCREEN_WIDTH-40)/3, 40)];
+        [wwwButton setTitle:@"www." forState:UIControlStateNormal];
+        [wwwButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [wwwButton addTarget:self action:@selector(addwww) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:wwwButton];
+        
+        UIButton *comButton = [[UIButton alloc]initWithFrame:CGRectMake(30+2*(SCREEN_WIDTH-40)/4, 5, (SCREEN_WIDTH-40)/3, 40)];
+        [comButton setTitle:@".com" forState:UIControlStateNormal];
+        [comButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [comButton addTarget:self action:@selector(addcom) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:comButton];
+        
+        UIButton *cnButton = [[UIButton alloc]initWithFrame:CGRectMake(40+3*(SCREEN_WIDTH-40)/4, 5, (SCREEN_WIDTH-40)/3, 40)];
+        [cnButton setTitle:@".cn" forState:UIControlStateNormal];
+        [cnButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [cnButton addTarget:self action:@selector(addcn) forControlEvents:UIControlEventTouchUpInside];
+        [aView addSubview:cnButton];
+        
+        self.inputAccessoryView=aView;
+        
+    }
+    return self;
+}
+
+
+
+
+- (void)addHttp{
+    self.text = @"http://";
+}
+- (void)addwww{
+    self.text = [self.text stringByAppendingString:@"www."];
+}
+- (void)addcom{
+    self.text = [self.text stringByAppendingString:@".com"];
+}
+- (void)addcn{
+    self.text = [self.text stringByAppendingString:@".cn"];
+}
+
+@end
